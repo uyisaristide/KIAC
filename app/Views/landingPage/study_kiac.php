07b1d36ab517d84d875865a321265d30690c8027
Anglebert Ish <82755721+Anglebert-Dev@users.noreply.github.com>
2023-09-11 12:11:48 +0200

    finalise forms

--- app/Views/landingPage/study_kiac.php
+++ app/Views/landingPage/study_kiac.php
@@ -384,7 +384,7 @@
       First Name: <input type="text" name="firstName" required><br>
       Last Name: <input type="text" name="lastName" required><br>
       Gender:
-      <input type="radio" name="gender" value="Male" required> Male
+      <input type="radio" name="want_to_study" value="Male" required> Male
       <input type="radio" name="gender" value="Female" required> Female
       <div class="error" id="genderError"></div>
       Nationality: <input type="text" name="nationality" required><br>
@@ -523,10 +523,78 @@
 
   <!-- Abroad Student Form -->
   <div class="form-container" id="abroadForm">
-    <h4>Abroad Student Form</h4>
-    <!-- Your form fields for abroad student go here -->
+    <div class="max-w-xl mx-auto mt-12">
+      <h2 class="text-gray-700 font-bold text-2xl">STUDY ABROAD FOR SCHOLARSHIP
+      </h2>
 
-  </div>
+      <div class="my-4">
+        <p class="text-gray-700 font-medium">KIAC is a professional educational agency, you can explore study abroad
+          opportunities with scholarships that cover 90% of your expenses. We specialize in providing guidance and
+          support to students interested in studying in Turkey, Armenia, Azerbaijan, and Schengen countries.</p>
+      </div>
+      <form id="abroadApplicationForm" enctype="multipart/form-data" METHOD="POST">
+
+        <h3>DO YOU WANT TO STUDY ABROAD FOR SCHOLARSHIP ?</h3>
+        <input type="radio" name="want_to_study" value="yes" required> Yes
+        <input type="radio" name="want_to_study" value="no" required> No
+        <!-- <div class="error" id="educationLevelError"></div> -->
+
+        <h3>IF YOU ARE INTERESTED ON THIS SCHOLARSHIP; PROCEED TO THE NEXT STEP.</h3>
+        <input type="radio" name="interested" value="YES" required>I AM INTERESTED
+        <input type="radio" name="interested" value="NO" required> NOT INTERESTED
+        <!-- <div class="error" id="secondaryFinishedError"></div> -->
+
+        <h3>PERSONAL INFORMATION</h3>
+        <!-- First Name: <input type="text" name="firstName" required><br>
+        Last Name: <input type="text" name="lastName" required><br> -->
+        Date of Birth: <input type="date" name="birth_date" required><br>
+        Gender:
+        <input type="radio" name="gender" value="Male" required> Male
+        <input type="radio" name="gender" value="Female" required> Female
+        <div class="error" id="genderError"></div>
+        Nationality: <input type="text" name="nationality" required><br>
+        Phone Number: <input type="tel" name="phone_number" required pattern="[0-9]{10}"><br>
+        Email Address: <input type="email" name="email_add" required><br>
+
+
+        <h3>YOUR CURRENT LEVEL OF EDUCATION </h3>
+        <input type="radio" name="university_level" value="University Level" required> University Level
+        <input type="radio" name="university_level" value="Secondary Level" required> Secondary Level
+        <div class="error" id="educationLevelError"></div>
+
+        <!-- ... Repeat for all other sections ... -->
+        <h3>SELECT YOUR DESIRED COUNTRY
+          *</h3>
+        <input type="radio" name="desired_country" value="TURKEY" required> TURKEY
+        <input type="radio" name="desired_country" value="ARMENIA" required> ARMENIA
+        <input type="radio" name="desired_country" value="AZERBAIJAN" required> AZERBAIJAN
+        <div class="error" id="universityGraduatedError"></div>
+
+        <h3>WHICH PROGRAM DO YOU WANT TO APPLY FOR ?
+          *</h3>
+        Program:
+        <input type="text" name="program" required><br>
+        <h3>REQUIRED DOCUMENT</h3>
+        YOUR PHOTO PASSPORT: <input type="file" name="passport_pic"><br>
+        ACADEMIC TRANSCRIPT [SCHOOL REPORT]: <input type="file" name="transcript_doc"><br>
+        YOUR VALID PASSPORT: <input type="file" name="id_passport"><br>
+        COVID_19 VACINATION CERTIFICATE <input type="file" name="vaccine"><br>
+
+        <input type="submit" value="Submit Application" id="submitButton">
+
+        <div id="myModal" class="modal">
+          <div class="modal-content">
+            <span class="close">&times;</span>
+            <p id="modalText"></p>
+          </div>
+        </div>
+
+      </form>
+
+
+      <!-- Your form fields for abroad student go here -->
+
+    </div>
 
 </body>
 
@@ -754,9 +822,9 @@
     fetch('http://localhost:3000/api/agents/application', {
       // fetch('http://173.212.230.165:3000/api/agents/application', {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
+      // headers: {
+      //   'Content-Type': 'application/x-www-form-urlencoded'
+      // },
       body: formData
     }).then(response => {
       if (!response.ok) {
@@ -782,10 +850,93 @@
     sendAgentDataToServer();
   });
 
+
+
+
+  // abbroad
+  function gatherAbroadFormData() {
+    let formData = new FormData();
+    formData.append('university_level', document.querySelector('[name="university_level"]:checked').value);
+    formData.append('gender', document.querySelector('[name="gender"]:checked').value);
+    formData.append('want_to_study', document.querySelector('[name="want_to_study"]:checked').value);
+    formData.append('interested', document.querySelector('[name="interested"]:checked').value);
+    formData.append('desired_country', document.querySelector('[name="desired_country"]:checked').value);
+    formData.append('birth_date', document.querySelector('[name="birth_date"]').value);
+    formData.append('phone_number', document.querySelector('[name="phone_number"]').value);
+    formData.append('email_add', document.querySelector('[name="email_add"]').value);
+    formData.append('program', document.querySelector('[name="program"]').value);
+
+    // Handle file uploads
+    if (document.querySelector('[name="id_passport"]').files.length > 0) {
+      formData.append('id_passport', document.querySelector('[name="id_passport"]').files[0]);
+    }
+    if (document.querySelector('[name="passport_pic"]').files.length > 0) {
+      formData.append('passport_pic', document.querySelector('[name="passport_pic"]').files[0]);
+    }
+    if (document.querySelector('[name="vaccine"]').files.length > 0) {
+      formData.append('vaccine', document.querySelector('[name="vaccine"]').files[0]);
+    }
+
+    if (document.querySelector('[name="transcript_doc"]').files.length > 0) {
+      formData.append('transcript_doc', document.querySelector('[name="transcript_doc"]').files[0]);
+    }
+    return formData;
+  }
+
+  function sendAbroadDataToServer() {
+
+    let formData = gatherAbroadFormData();
+
+    fetch('http://localhost:3000/api/study/abroad/application', {
+      method: 'POST',
+      headers: {
+        // 'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: formData,
+    })
+      .then((response) => {
+        if (!response.ok) {
+          return response.json().then((data) => {
+            const errorMessage = data.error || 'Please fill the form correctly';
+            alertAndLogError(errorMessage);
+            throw new Error(errorMessage);
+          });
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // alertAndReload('Your application was sent successfully!');
+      })
+      .catch((error) => {
+        console.error('Client-Side Error:', error);
+        alertAndLogError('Something went wrong! Please fill the form correctly.');
+      });
+  }
+
+  function alertAndLogError(errorMessage) {
+    alert(errorMessage);
+    console.error('Server-Side Error:', errorMessage);
+  }
+
+  // function alertAndReload(message) {
+  //   alert(message);
+  //   // Optionally reload the page or perform other actions
+  //   // window.location.reload();
+  // }
+
+  document.getElementById('abroadApplicationForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    sendAbroadDataToServer();
+  });
+
+
   function alertAndReload(message) {
     alert(message);
-    location.reload();
+    // location.reload();
   }
+
+
 
 </script>
 
